0e2dffc8a2076247ca2c22fea71ac46b68e2520e
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-18 14:34:09 +0300

    Create database configurations

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const connection_String = 'mongodb://127.0.0.1:27017/GamingTeam';
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(connection_String);
+        console.log('Database is connecting');
+    } catch (err) {
+        console.log(err.message);
+        process.exit('1');
+    }
+
+}
