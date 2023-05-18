16e13729de48854815476bbab7b2fb2c0a2efb4a
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-18 14:33:50 +0300

    Create index

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const databaseConfig = require('./config/database');
+const expressConfig = require('./config/express');
+const routsConfig = require('./config/routs');
+
+const port = 3000;
+
+
+async function start() {
+    const app = express();
+
+    await databaseConfig(app);
+    expressConfig(app);
+    routsConfig(app);
+
+    app.listen(port, () => console.log(`Server starts on port ${port}!`));
+}
+
+start();
