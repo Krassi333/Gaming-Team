6a02f1e17ee88d8067fbae583f6e40eafffeea84
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-19 16:51:01 +0300

    Create express configurations

--- config/express.js
+++ config/express.js
@@ -5,11 +5,11 @@ const session = require('../midllewares/session');
 
 module.exports = (app) => {
     const hbs = handlebars.create({
-        extname: 'hbs'
+        extname: '.hbs'
     });
 
-    app.engine('hbs', hbs.engine);
-    app.set('view engine', 'hbs');
+    app.engine('.hbs', hbs.engine);
+    app.set('view engine', '.hbs');
 
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
