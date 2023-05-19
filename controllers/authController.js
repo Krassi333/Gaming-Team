be294f2340ea562e6a067cfb2e09ae6c1e018cc8
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-19 16:55:30 +0300

    Create register functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,4 +10,30 @@ router.get('/register', isGuest(),async (req, res) => {
     })
 });
 
+router.post('/register',  isGuest(),async (req, res) => {
+    const data = req.body;
+
+    try {
+        if (data.password != data.repass) {
+            throw new Error('Passwords don\'t match !');
+        }
+
+        const token = await register(data.username, data.email, data.password);
+
+        res.cookie('token', token);
+
+        res.redirect('/');
+
+    } catch (err) {
+        const errors = errorParser(err);
+
+        res.render('register', {
+            title: 'Register Page',
+            errors,
+            data
+
+        })
+    }
+});
+
 module.exports = router;
