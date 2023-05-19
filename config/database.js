4de26363138a494a9e3cf6e053a11d9c7ed89f20
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-19 16:43:02 +0300

    Add database configuration

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const connection_String = 'mongodb://127.0.0.1:27017/GamingTeam';
 
 module.exports = async (app) => {
     try {
-        await mongoose.connect(connection_String);
+        await mongoose.connect(connection_String,{
+            useNewUrlParser:true,
+            useUnifiedTopology: true
+        });
         console.log('Database is connecting');
     } catch (err) {
         console.log(err.message);
